dad73c2f59df6976c466e6fc8eda3e620f746b66
Omar M. Osman <omarosmandev@gmail.com>
2021-02-11 01:12:18 +0200

    add debug starting script

--- src/models/User.js
+++ src/models/User.js
@@ -27,6 +27,10 @@ const userSchema = mongoose.Schema({
         required: true,
         minLength: 7
     },
+    verified: {
+        type: Boolean,
+        default: false
+    },
     tokens: [{
         token: {
             type: String,
