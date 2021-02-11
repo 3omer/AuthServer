994a8c968e3bea34645bd144945098944cc6dfad
Omar M. Osman <omarosmandev@gmail.com>
2021-02-11 22:13:48 +0200

    start the app from index file

--- src/app.js
+++ src/app.js
@@ -4,13 +4,9 @@ const app = express()
 require("./db/db")
 const userRouter = require("./routers/user")
 
-const PORT = process.env.PORT
-
 app.use(express.json())
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
 
 // register routers
 app.use(userRouter)
+
 module.exports = app
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const app = require('./app')
+
+const PORT = process.env.PORT
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
