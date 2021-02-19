f35122ddb50cb2426b9db9a6c78f4214e47c0d49
Omar M. Osman <omarosmandev@gmail.com>
2021-02-19 14:32:04 +0200

    add morga

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-const express = require("express")
 require("dotenv").config()
+const express = require("express")
 const app = express()
+const morgan = require("morgan")
 require("./db/db")
 const userRouter = require("./routers/user")
 
 app.use(express.json())
-
+app.use(morgan("dev"))
 // register routers
 app.use(userRouter)
 
