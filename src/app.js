490ab61a9274201e53fa8af7d06d32a6afd0753a
Omar M. Osman <omarosmandev@gmail.com>
2021-03-03 02:18:27 +0200

    catch unhandeled errors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ const app = express()
 const morgan = require("morgan")
 require("./db/db")
 const userRouter = require("./routers/user")
+const logger = require("./utils").logger
 
 app.use(express.json())
 app.use(morgan("dev"))
 // register routers
 app.use(userRouter)
+
+// error handler
+app.use((error, req, res, next) => {
+    logger.error(error)
+    res.status(500).json({ error: 'Something went wrong' })
+}) 
 
 module.exports = app
